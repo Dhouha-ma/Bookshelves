77bd26142b9212bba17b75f0b969a383c1e98a47
dhouha <dhouha.mansour11@gmail.com>
2021-09-22 16:13:21 +0200

    signup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor(){
+    var firebaseConfig = {
+      apiKey: "AIzaSyAX6eXSOtM4fHBDqHMSZscW2aGDSY19i18",
+      authDomain: "bookshelves-c8575.firebaseapp.com",
+      projectId: "bookshelves-c8575",
+      storageBucket: "bookshelves-c8575.appspot.com",
+      messagingSenderId: "1092347072963",
+      appId: "1:1092347072963:web:a45ac6681f12745f33ed76",
+      measurementId: "G-RPGJL0MJZG"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
