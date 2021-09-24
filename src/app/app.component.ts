8f5ed7c843dd5ea079371df1f45ae7ffadd05d3f
dhouha <dhouha.mansour11@gmail.com>
2021-09-24 11:53:45 +0200

    books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,15 @@ import * as firebase from 'firebase';
 })
 export class AppComponent {
   constructor(){
-    var firebaseConfig = {
+    const firebaseConfig = {
       apiKey: "AIzaSyAX6eXSOtM4fHBDqHMSZscW2aGDSY19i18",
       authDomain: "bookshelves-c8575.firebaseapp.com",
+      databaseURL: "https://bookshelves-c8575.firebaseio.com",
       projectId: "bookshelves-c8575",
       storageBucket: "bookshelves-c8575.appspot.com",
       messagingSenderId: "1092347072963",
-      appId: "1:1092347072963:web:a45ac6681f12745f33ed76",
-      measurementId: "G-RPGJL0MJZG"
+      // appId: "1:1092347072963:web:a45ac6681f12745f33ed76",
+      // measurementId: "G-RPGJL0MJZG"
     };
     firebase.initializeApp(firebaseConfig);
   }
--- src/app/models/book.model.ts
+++ src/app/models/book.model.ts
@@ -1,6 +1,4 @@
 export class Book {
     photo: string;
-    constructor(public title: string, public author: string) {
-        
-    }
+    constructor(public title: string, public author: string) {}
 }
